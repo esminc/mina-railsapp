4e5c14c759e2dd8ee7fdbb13d2f931c35bffd7e8
dot <shuhei.kondo@gmail.com>
2013-03-19 21:25:26 +0900

    bundle gem

---
+++ lib/mina/railsapp.rb
@@ -0,0 +1,7 @@
+require "mina/railsapp/version"
+
+module Mina
+  module Railsapp
+    # Your code goes here...
+  end
+end
---
+++ lib/mina/railsapp/version.rb
@@ -0,0 +1,5 @@
+module Mina
+  module Railsapp
+    VERSION = "0.0.1"
+  end
+end
